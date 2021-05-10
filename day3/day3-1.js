5a2088f7b9a15513e1e06e79f634e81e44ae33f2
prajapatilalit <prajapatilalit150992@gmail.com>
2021-05-11 00:27:37 +0530

    add a skills set in newStudent object

---
+++ day3/day3-1.js
@@ -0,0 +1,15 @@
+// const student = new Map([
+//   ["name", "David"],
+//   ["skills", ["HTM", "CSS", "JS", "React"]],
+//   ["scores", [98, 85, 90, 95]],
+// ]);
+
+// // const obj = Object.fromEntries(student);
+// // console.log(obj);
+// // const data = ["name", "skills", "scores"];
+// // const student = ["David", ["HTM", "CSS", "JS", "React"], [98, 85, 90, 95]];
+
+// const convertArrayToObject = (arr) => {
+//   console.log(Object.fromEntries(arr));
+// };
+// convertArrayToObject(student);
---
+++ day3/day3-1a.js
@@ -0,0 +1,25 @@
+const student = {
+  name: "David",
+  age: 25,
+  skills: {
+    frontEnd: [
+      { skill: "HTML", level: 10 },
+      { skill: "CSS", level: 8 },
+      { skill: "JS", level: 8 },
+      { skill: "React", level: 9 },
+    ],
+    backEnd: [
+      { skill: "Node", level: 7 },
+      { skill: "GraphQL", level: 8 },
+    ],
+    dataBase: [{ skill: "MongoDB", level: 7.5 }],
+    dataScience: ["Python", "R", "D3.js"],
+  },
+};
+
+// console.log(student);
+
+const newStudent = Object.assign({}, student);
+newStudent.skills.frontEnd = { skill: "bootstrap", level: 8 };
+console.log(newStudent);
+console.log(student);
