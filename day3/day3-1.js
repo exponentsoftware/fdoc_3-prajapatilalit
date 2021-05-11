7dacc4bca903ed82564c4f7eb2852e62bfc8aac9
prajapatilalit <prajapatilalit150992@gmail.com>
2021-05-11 11:02:35 +0530

    resolve conflict in file 3-1 and 3-1a

--- day3/day3-1.js
+++ day3/day3-1.js
@@ -4,18 +4,3 @@ const [, , jsScore, reactScore] = scores;
 console.log(name, skills, scores);
 
 console.log(jsScore, reactScore);
-// const student = new Map([
-//   ["name", "David"],
-//   ["skills", ["HTM", "CSS", "JS", "React"]],
-//   ["scores", [98, 85, 90, 95]],
-// ]);
-
-// // const obj = Object.fromEntries(student);
-// // console.log(obj);
-// // const data = ["name", "skills", "scores"];
-// // const student = ["David", ["HTM", "CSS", "JS", "React"], [98, 85, 90, 95]];
-
-// const convertArrayToObject = (arr) => {
-//   console.log(Object.fromEntries(arr));
-// };
-// convertArrayToObject(student);
